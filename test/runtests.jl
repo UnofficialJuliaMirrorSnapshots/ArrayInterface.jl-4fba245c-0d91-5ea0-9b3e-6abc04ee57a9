22b571192f79695377983bf15e2f197582f681ce
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-08-07 09:35:56 +0000

    Snapshot of branch master taken on 2019-08-07T05:35:55.077-04:00 from "https://github.com/UnofficialJuliaMirror/ArrayInterface.jl-4fba245c-0d91-5ea0-9b3e-6abc04ee57a9"

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using ArrayInterface, Test
+using Base: setindex
 import ArrayInterface: has_sparsestruct, findstructralnz
 @test ArrayInterface.ismutable(rand(3))
 
@@ -37,3 +38,84 @@ Sp=sparse([1,2,3],[1,2,3],[1,2,3])
 @test has_sparsestruct(Sp)
 rowind,colind=findstructralnz(Sp)
 @test [Tri[rowind[i],colind[i]] for i in 1:length(rowind)]==[1,2,3]
+
+using BandedMatrices
+
+B=BandedMatrix(Ones(5,5), (-1,2))
+B[band(1)].=[1,2,3,4]
+B[band(2)].=[5,6,7]
+@test has_sparsestruct(B)
+rowind,colind=findstructralnz(B)
+@test [B[rowind[i],colind[i]] for i in 1:length(rowind)]==[5,6,7,1,2,3,4]
+B=BandedMatrix(Ones(4,6), (-1,2))
+B[band(1)].=[1,2,3,4]
+B[band(2)].=[5,6,7,8]
+rowind,colind=findstructralnz(B)
+@test [B[rowind[i],colind[i]] for i in 1:length(rowind)]==[5,6,7,8,1,2,3,4]
+
+using BlockBandedMatrices
+BB=BlockBandedMatrix(Ones(10,10),([1,2,3,4],[4,3,2,1]),(1,0))
+BB[Block(1,1)].=[1 2 3 4]
+BB[Block(2,1)].=[5 6 7 8;9 10 11 12]
+rowind,colind=findstructralnz(BB)
+@test [BB[rowind[i],colind[i]] for i in 1:length(rowind)]==
+    [1,5,9,2,6,10,3,7,11,4,8,12,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1]
+
+dense=collect(Ones(8,8))
+for i in 1:8
+    dense[:,i].=[1,2,3,4,5,6,7,8]
+end
+BBB=BandedBlockBandedMatrix(dense, ([4, 4] ,[4, 4]), (1, 1), (1, 1))
+rowind,colind=findstructralnz(BBB)
+@test [BBB[rowind[i],colind[i]] for i in 1:length(rowind)]==
+    [1,2,3,1,2,3,4,2,3,4,5,6,7,5,6,7,8,6,7,8,
+     1,2,3,1,2,3,4,2,3,4,5,6,7,5,6,7,8,6,7,8]
+
+@testset "setindex" begin
+    @testset "$(typeof(x))" for x in [
+        zeros(3),
+        falses(3),
+        spzeros(3),
+    ]
+        y = setindex(x, true, 1)
+        @test iszero(x)  # x is not mutated
+        @test y[1] == true
+        @test iszero(x[CartesianIndices(size(x)) .== [CartesianIndex(1)]])
+
+        y2 = setindex(x, one.(x), :)
+        @test iszero(x)
+        @test all(isone, y2)
+    end
+
+    @testset "$(typeof(x))" for x in [
+        zeros(3, 3),
+        falses(3, 3),
+        spzeros(3, 3),
+    ]
+        y = setindex(x, true, 1, 1)
+        @test iszero(x)  # x is not mutated
+        @test y[1, 1] == true
+        @test iszero(x[CartesianIndices(size(x)) .== [CartesianIndex(1, 1)]])
+
+        y2 = setindex(x, one.(x), :, :)
+        @test iszero(x)
+        @test all(isone, y2)
+    end
+
+    @testset "$(typeof(x))" for x in [
+        zeros(3, 3, 3),
+        falses(3, 3, 3),
+    ]
+        y = setindex(x, true, 1, 1, 1)
+        @test iszero(x)  # x is not mutated
+        @test y[1, 1, 1] == true
+        @test iszero(x[CartesianIndices(size(x)) .== [CartesianIndex(1, 1, 1)]])
+
+        y2 = setindex(x, one.(x), :, :, :)
+        @test iszero(x)
+        @test all(isone, y2)
+    end
+end
